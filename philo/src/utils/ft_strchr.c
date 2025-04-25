1cf8c1f76115afb350ff90a8084b197364dbcd2b
ghambrec <ghambrec@student.42heilbronn.de>
2025-04-25 16:13:24 +0200

    basis built

---
+++ philo/src/utils/ft_strchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 10:12:37 by ghambrec          #+#    #+#             */
+/*   Updated: 2025/04/25 15:37:57 by ghambrec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	char	ch;
+
+	ch = (char)c;
+	while (*str != '\0')
+	{
+		if (*str == ch)
+		{
+			return ((char *)str);
+		}
+		str++;
+	}
+	if (ch == '\0')
+	{
+		return ((char *)str);
+	}
+	return (NULL);
+}
