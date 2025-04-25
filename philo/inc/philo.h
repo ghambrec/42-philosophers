b5cd979b35b8dcaec383b83b3b9e7bbad5ad3fea
ghambrec <ghambrec@student.42heilbronn.de>
2025-04-25 19:06:52 +0200

    started with check_arguments

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,21 +6,29 @@
 /*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 14:13:31 by ghambrec          #+#    #+#             */
-/*   Updated: 2025/04/25 15:39:20 by ghambrec         ###   ########.fr       */
+/*   Updated: 2025/04/25 18:59:50 by ghambrec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <stdio.h>
+// weil stdlib nicht inkludiert ist
+# define	EXIT_FAILURE 1
+# define	EXIT_SUCCESS 0
+
+// unnoetige includes wieder loeschen
+# include <stdio.h> // printf
+# include <pthread.h> // thread
+# include <unistd.h> // usleep
 
 
 // ---------------------------------------------
 // UTILS
 // ---------------------------------------------
-int	ft_atoi(const char *str);
-int	ft_isdigit(int num);
+int		ft_atoi(const char *str);
+int		ft_isdigit(int num);
 char	*ft_strchr(const char *str, int c);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -5,15 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/25 14:13:56 by ghambrec          #+#    #+#             */
-/*   Updated: 2025/04/25 16:10:48 by ghambrec         ###   ########.fr       */
+/*   Created: 2025/04/25 18:53:42 by ghambrec          #+#    #+#             */
+/*   Updated: 2025/04/25 19:06:27 by ghambrec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(void)
+int	check_arguments(int argc, char **argv)
 {
-	printf("Hallo!\n");
-	return (1);
+	int	i;
+	if (argc < 5 || argc > 6)
+	{
+		ft_putendl_fd("Invalid number of arguments!", STDERR_FILENO);
+		return (EXIT_FAILURE);
+	}
+	i = 1;
+	while (argv[i])
+	{ // kleiner 0 oder kleiner gleich 0 ??? deckel aus eval sheet mit einbauen??
+		if (ft_atoi(argv[i]) < 0)
+		{
+			ft_putendl_fd("Only positive numbers allowed!", STDERR_FILENO);
+		}
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (check_arguments(argc, argv) != 0)
+		return (EXIT_FAILURE);	
+
+
+	return (EXIT_SUCCESS);
 }
