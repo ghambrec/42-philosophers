8e8936f7d71f06edfc0b9a6a6b5430dfe5a259f7
ghambrec <ghambrec@student.42heilbronn.de>
2025-04-28 17:28:43 +0200

    done for today

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 18:53:42 by ghambrec          #+#    #+#             */
-/*   Updated: 2025/04/28 17:06:31 by ghambrec         ###   ########.fr       */
+/*   Updated: 2025/04/28 17:28:34 by ghambrec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,12 @@ int	main(int argc, char **argv)
 	if (check_arguments(argc, argv) != 0)
 		return (EXIT_FAILURE);
 	init_structs(&table, argc, argv);
-	
+	if (create_philos(&table) != 0) // mit der funktion weitermachen
+	{
+		ft_putendl_fd("Error creating philo-threads\n", STDERR_FILENO);
+		return (EXIT_FAILURE);
+	}
+	// fuer monitor noch separate pthread_t in table struct 
 	
 	return (EXIT_SUCCESS);
 }
