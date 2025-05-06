66d51cdc5239ca9667e819650444693a6a7ffe6c
ghambrec <ghambrec@student.42heilbronn.de>
2025-05-06 17:11:02 +0200

    new error msgs

--- philo/src/check_arguments.c
+++ philo/src/check_arguments.c
@@ -6,7 +6,7 @@
 /*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 13:30:03 by ghambrec          #+#    #+#             */
-/*   Updated: 2025/04/28 15:18:18 by ghambrec         ###   ########.fr       */
+/*   Updated: 2025/05/06 17:10:55 by ghambrec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	check_arguments(int argc, char **argv)
 	int	i;
 	if (argc < 5 || argc > 6)
 	{
-		ft_putendl_fd("Invalid number of arguments!", STDERR_FILENO);
+		ft_putendl_fd("Invalid input - Wrong number of arguments!", STDERR_FILENO);
 		return (1);
 	}
 	i = 1;
@@ -36,16 +36,16 @@ int	check_arguments(int argc, char **argv)
 	{
 		if (ft_atoi(argv[i]) <= 0)
 		{
-			ft_putendl_fd("Only numbers bigger than 0 allowed!", STDERR_FILENO);
+			ft_putendl_fd("Invalid input - Only numbers bigger than 0 allowed!", STDERR_FILENO);
 			return (1);
 		}
 		if (contains_only_numbers(argv[i]) == false)
 		{
-			ft_putendl_fd("Only numbers allowed!", STDERR_FILENO);
+			ft_putendl_fd("Invalid input - Only numbers allowed!", STDERR_FILENO);
 			return (1);
 		}
 		if (i == 1 && ft_atoi(argv[i]) > MAX_PHILOS)
-			return (ft_putendl_fd("Max philo is 200", STDERR_FILENO), 1);
+			return (ft_putendl_fd("Invalid input - Max philo is 200", STDERR_FILENO), 1);
 		i++;
 	}
 	return (0);
