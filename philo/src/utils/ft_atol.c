b810e97d99767b6360503731d01ce8194c3dd774
ghambrec <ghambrec@student.42heilbronn.de>
2025-05-07 12:11:39 +0200

    added function files for prev commit

---
+++ philo/src/utils/ft_atol.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 18:26:27 by ghambrec          #+#    #+#             */
+/*   Updated: 2025/05/07 12:07:38 by ghambrec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+long	ft_atol(const char *str)
+{
+	int		i;
+	long	num;
+	int		neg;
+
+	i = 0;
+	num = 0;
+	neg = 0;
+	if (!str[i])
+		return (0);
+	while (ft_strchr(" \t\n\r\v\f", str[i]) != NULL)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg = 1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		num = num * 10 + (str[i] - 48);
+		i++;
+	}
+	if (neg == 1)
+		num *= -1;
+	return (num);
+}
